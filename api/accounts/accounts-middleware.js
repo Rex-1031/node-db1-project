e1126e2548a7649d1ad6825b4956b0ee4d8b2519
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-08-18 16:30:42 -0400

    accounts middlware created

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,66 @@
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { name, budget, id} = req.body
+  
+  if(!budget || !budget.trim() || !name || !name.trim()){
+    res.status(400).json({
+      message: '"name and budget are required"'
+    })
+  
+    if(typeof name  !== 'string'){
+    res.status(400).json({
+       message: "name of account must be a string"
+    })
+    }
+    if(name.trim()< 3 || name.trim() > 100){
+      res.status(400).json({
+        message: "name of account must be between 3 and 100"
+      })
+    }
+    if(typeof budget !== 'number' ){
+      res.status(400).json({
+        message: "budget of account must be a number"
+      })
+    }
+    if(budget < 0 || budget > 1000000){
+      res.status(400).json({
+        message: "budget of account is too large or too small"
+      })
+    }
+  }else{
+    req.name = name.trim()
+    req.budget = budget
+    req.id =id
+    next()
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  const accountName = await Accounts.get(req.body.name)
+  if (accountName){
+    res.status(400).json({
+      message: "that name is taken"
+    })
+  }else{
+    req.accountName = accountName
+    next()
+  }
+  
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const accounts = await Accounts.get(req.params.id)
+    if(!accounts){
+      res.status(404).json({
+        message: "account not found"
+      })
+    }else{
+      req.accounts = accounts
+      next()
+    }
+    
+  }
+  catch(err){
+    next(err)
+  }
 }
